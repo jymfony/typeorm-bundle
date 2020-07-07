cea7b293d09966192e04df78bcf798b797c501f0
Alessandro Chitolina <alekitto@gmail.com>
2020-07-07 18:04:30 +0200

    merge metadata from decorators with global metadata

--- src/Metadata/ConnectionMetadataBuilder.js
+++ src/Metadata/ConnectionMetadataBuilder.js
@@ -34,6 +34,37 @@ export default class ConnectionMetadataBuilder extends Base {
         const metadataArgsStorageFromSchema = new EntitySchemaTransformer().transform(entitySchemas);
         const schemaEntityMetadatas = new EntityMetadataBuilder(this.connection, metadataArgsStorageFromSchema).build();
 
+        this._mergeMetadataArgs(getMetadataArgsStorage(), metadataArgsStorageFromSchema);
+
         return [ ...decoratorEntityMetadatas, ...schemaEntityMetadatas ];
+    }
+
+    _mergeMetadataArgs(...args) {
+        /** @type {MetadataArgsStorage} */
+        const target = args.shift();
+        for (const storage of args) {
+            target.tables = [...target.tables, ...storage.tables];
+            target.trees = [...target.trees, ...storage.trees];
+            target.entityRepositories = [...target.entityRepositories, ...storage.entityRepositories];
+            target.transactionEntityManagers = [...target.transactionEntityManagers, ...storage.transactionEntityManagers];
+            target.transactionRepositories = [...target.transactionRepositories, ...storage.transactionRepositories];
+            target.namingStrategies = [...target.namingStrategies, ...storage.namingStrategies];
+            target.entitySubscribers = [...target.entitySubscribers, ...storage.entitySubscribers];
+            target.indices = [...target.indices, ...storage.indices];
+            target.uniques = [...target.uniques, ...storage.uniques];
+            target.checks = [...target.checks, ...storage.checks];
+            target.exclusions = [...target.exclusions, ...storage.exclusions];
+            target.columns = [...target.columns, ...storage.columns];
+            target.generations = [...target.generations, ...storage.generations];
+            target.relations = [...target.relations, ...storage.relations];
+            target.joinColumns = [...target.joinColumns, ...storage.joinColumns];
+            target.joinTables = [...target.joinTables, ...storage.joinTables];
+            target.entityListeners = [...target.entityListeners, ...storage.entityListeners];
+            target.relationCounts = [...target.relationCounts, ...storage.relationCounts];
+            target.relationIds = [...target.relationIds, ...storage.relationIds];
+            target.embeddeds = [...target.embeddeds, ...storage.embeddeds];
+            target.inheritances = [...target.inheritances, ...storage.inheritances];
+            target.discriminatorValues = [...target.discriminatorValues, ...storage.discriminatorValues];
+        }
     }
 }
